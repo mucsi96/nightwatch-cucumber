cebd43857a40ed22e28ff8fc0a7edb06e4935355
Igor Mucsicska <mucsi_96@yahoo.com>
2016-08-19 08:56:30 +0200

    test(multi feature): mocha based fulti feature test added

--- test/nighwatch-runner.test.js
+++ test/nighwatch-runner.test.js
@@ -31,6 +31,52 @@ describe('Nightwatch runner', () => {
       })
   })
 
+  it.only('should handle multi feature tests', () => {
+    return testCaseFactory
+      .create('simpleTest')
+      .feature('adition')
+      .scenario('small numbers')
+      .given('User is on the simple calculator page', function () {this.init()})
+      .and('User enter 4 in A field', function () {this.setValue('#a', 4)})
+      .and('User enter 5 in B field', function () {this.setValue('#b', 5)})
+      .when('User press Add button', function () {this.click('#add')})
+      .then('The result should contain 9', function () {this.assert.containsText('#result', 9)})
+      .scenario('big numbers')
+      .given('User is on the simple calculator page')
+      .and('User enter 4 in A field')
+      .and('User enter 5 in B field')
+      .when('User press Add button')
+      .then('The result should contain 9')
+      .feature('subtraction')
+      .scenario('small numbers')
+      .given('User is on the simple calculator page')
+      .and('User enter 9 in A field', function () {this.setValue('#a', 9)})
+      .and('User enter 3 in B field', function () {this.setValue('#b', 3)})
+      .when('User press Subtract button', function () {this.click('#subtract')})
+      .then('The result should contain 6', function () {this.assert.containsText('#result', 6)})
+      .scenario('big numbers')
+      .given('User is on the simple calculator page')
+      .and('User enter 4 in A field')
+      .and('User enter 5 in B field')
+      .when('User press Subtract button')
+      .then('The result should contain -1', function () {this.assert.containsText('#result', -1)})
+      .run()
+      .then((features) => {
+        features[0].result.status.should.be.passed
+        features[0].result.scenarioCounts.should.deep.equal({passed: 2})
+        features[0].scenarios[0].result.status.should.be.passed
+        features[0].scenarios[0].result.stepCounts.should.deep.equal({passed: 5})
+        features[0].scenarios[1].result.status.should.be.passed
+        features[0].scenarios[1].result.stepCounts.should.deep.equal({passed: 5})
+        features[1].result.status.should.be.passed
+        features[1].result.scenarioCounts.should.deep.equal({passed: 2})
+        features[1].scenarios[0].result.status.should.be.passed
+        features[1].scenarios[0].result.stepCounts.should.deep.equal({passed: 5})
+        features[1].scenarios[1].result.status.should.be.passed
+        features[1].scenarios[1].result.stepCounts.should.deep.equal({passed: 5})
+      })
+  })
+
   it('should handle undefined steps', () => {
     return testCaseFactory
       .create('undefinedStepTest')
@@ -232,7 +278,7 @@ describe('Nightwatch runner', () => {
       })
   })
 
-  it.only('should handle test group skipping', () => {
+  it('should handle test group skipping', () => {
     return testCaseFactory
       .create('testGroupTest')
       .group('positive')
