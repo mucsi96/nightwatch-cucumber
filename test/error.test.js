571bd589c68080182e1af85b56af4eb353a954bb
Igor Mucsicska <mucsi_96@yahoo.com>
2016-09-17 19:16:31 +0200

    chore: show stack trace for failing step

--- test/error.test.js
+++ test/error.test.js
@@ -76,10 +76,16 @@ describe('Error handling', () => {
       .and('User enter 5 in B field', function () { this.setValue('#b', 5) })
       .when('User press Add button', function () { this.click('#add') })
       .then('The result should contain 8', function () { this.assert.containsText('#result', 8) })
+      .scenario('big numbers')
+      .given('User is on the simple calculator page')
+      .and('User enter 82 in A field', function () { this.setValue('#a', 82) })
+      .and('User enter 11 in B field', function () { this.setValue('#b', 11) })
+      .when('User press Add button')
+      .then('The result should contain 93', function () { this.assert.containsText('#result', 93) })
       .run()
       .then((result) => {
         result.features[0].result.status.should.be.failed
-        result.features[0].result.scenarioCounts.should.deep.equal({failed: 1})
+        result.features[0].result.scenarioCounts.should.deep.equal({failed: 1, passed: 1})
         result.features[0].scenarios[0].result.status.should.be.failed
         result.features[0].scenarios[0].result.stepCounts.should.deep.equal({failed: 1, passed: 4})
       })
@@ -109,7 +115,7 @@ describe('Error handling', () => {
       .and('User enter 4 in A field', function () { this.setValue('#a', 4) })
       .and('User enter 5 in B field', function () { this.setValue('#b', 5) })
       .when('User press Add button', function () { this.click('#add') })
-      .then('The result should contain 9', function () { this.assert.containsText('#result', 8) })
+      .then('The result should contain 9', function () { this.assert.containsText('#result', 9) })
       .scenario('big numbers')
       .given('User is on the simple calculator page', function () { this.init() })
       .and('User enter 82 in A field', function () { this.setValue('#a', 82) })
