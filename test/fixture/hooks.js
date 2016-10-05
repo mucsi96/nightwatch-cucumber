95c7501004d97e966760d01319d379897cd01389
Igor Mucsicska <mucsi_96@yahoo.com>
2016-10-05 21:43:03 +0200

    feat: Provide Nightwatch client api as context for Cucumber hooks. Closes #111

--- test/fixture/hooks.js
+++ test/fixture/hooks.js
@@ -1,40 +1,52 @@
+/* eslint-env mocha */
+const chai = require('chai')
+chai.should()
+
 module.exports = function () {
   this.registerHandler('BeforeFeatures', function (features, cb) {
+    this.should.have.ownProperty('click')
     if (process.send) process.send('<F')
     cb()
   })
 
   this.registerHandler('BeforeFeature', function (feature, cb) {
+    this.should.have.ownProperty('click')
     if (process.send) process.send('<f')
     cb()
   })
 
   this.registerHandler('BeforeScenario', function (scenario, cb) {
+    this.should.have.ownProperty('click')
     if (process.send) process.send('<S')
     cb()
   })
 
   this.registerHandler('BeforeStep', function (step, cb) {
+    this.should.have.ownProperty('click')
     if (process.send) process.send('<s')
     cb()
   })
 
   this.registerHandler('AfterStep', function (step, cb) {
+    this.should.have.ownProperty('click')
     if (process.send) process.send('s>')
     cb()
   })
 
   this.registerHandler('AfterScenario', function (scenario, cb) {
+    this.should.have.ownProperty('click')
     if (process.send) process.send('S>')
     cb()
   })
 
   this.registerHandler('AfterFeature', function (feature, cb) {
+    this.should.have.ownProperty('click')
     if (process.send) process.send('f>')
     cb()
   })
 
   this.registerHandler('AfterFeatures', function (features, cb) {
+    this.should.have.ownProperty('click')
     if (process.send) process.send('F>')
     cb()
   })
