27bf6f4fc2c03c7e4108936f212113a2dd27c26c
myklt <maiklasm@gmail.com>
2017-02-11 21:47:06 +0100

    fix: promisify page expectation

--- test/client.test.js
+++ test/client.test.js
@@ -95,4 +95,32 @@ describe('Client features', () => {
         result.features[0].scenarios[0].result.stepCounts.should.deep.equal({passed: 4, failed: 1})
       })
   })
+
+  it('should handle nightwatch page expect.element', () => {
+    return testCaseFactory
+      .create('page-expect-element-test')
+      .pageObject('calculator', `module.exports = {
+  elements: {
+    result: '#result'
+  }
+}`)
+      .feature('addition')
+      .scenario('small numbers')
+      .given('User is on the simple calculator page', () => client.init())
+      .and('User enter 4 in A field', () => client.setValue('#a', 4))
+      .and('User enter 5 in B field', () => client.setValue('#b', 5))
+      .when('User press Add button', () => client.click('#add'))
+      .then('The result should contain 10', () => {
+        const calculator = client.page.calculator()
+        console.log(calculator.expect)
+        return calculator.expect.element('@result').text.to.contain(10)
+      })
+      .run()
+      .then((result) => {
+        result.features[0].result.status.should.be.failed
+        result.features[0].result.scenarioCounts.should.deep.equal({failed: 1})
+        result.features[0].scenarios[0].result.status.should.be.failed
+        result.features[0].scenarios[0].result.stepCounts.should.deep.equal({passed: 4, failed: 1})
+      })
+  })
 })
