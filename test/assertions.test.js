90ee2435d6c6c4c7ee7baaddfa19457b0e53771b
Igor Muchychka <mucsi_96@yahoo.com>
2017-09-02 12:28:16 +0200

    Improve session closing prevention

--- test/assertions.test.js
+++ test/assertions.test.js
@@ -96,9 +96,9 @@ describe('Assertion features', () => {
       })
   })
 
-  it('should handle nightwatch page object expect.element', () => {
+  it('should handle nightwatch page object expect.element failure', () => {
     return testCaseFactory
-      .create('page-expect-element-test')
+      .create('page-expect-element-failure-test')
       .pageObject('calculator', `module.exports = {
   elements: {
     result: '#result'
@@ -120,6 +120,33 @@ describe('Assertion features', () => {
         result.features[0].result.scenarioCounts.should.deep.equal({failed: 1})
         result.features[0].scenarios[0].result.status.should.be.failed
         result.features[0].scenarios[0].result.stepCounts.should.deep.equal({passed: 4, failed: 1})
+      })
+  })
+
+  it('should handle nightwatch page object expect.element success', () => {
+    return testCaseFactory
+      .create('page-expect-element-success-test')
+      .pageObject('calculator', `module.exports = {
+  elements: {
+    result: '#result'
+  }
+}`)
+      .feature('addition')
+      .scenario('small numbers')
+      .given('User is on the simple calculator page', () => client.init())
+      .and('User enter 4 in A field', () => client.setValue('#a', 4))
+      .and('User enter 5 in B field', () => client.setValue('#b', 5))
+      .when('User press Add button', () => client.click('#add'))
+      .then('The result should contain 9', () => {
+        const calculator = client.page.calculator()
+        return calculator.expect.element('@result').text.to.contain(9)
+      })
+      .run()
+      .then((result) => {
+        result.features[0].result.status.should.be.passed
+        result.features[0].result.scenarioCounts.should.deep.equal({passed: 1})
+        result.features[0].scenarios[0].result.status.should.be.passed
+        result.features[0].scenarios[0].result.stepCounts.should.deep.equal({passed: 5})
       })
   })
 
