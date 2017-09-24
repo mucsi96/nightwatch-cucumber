d128be9fd6d729189866a7db1af8fbceb64706f4
Igor Muchychka <mucsi_96@yahoo.com>
2017-09-24 14:32:54 +0200

    Improve tests

--- test/utility.test.js
+++ test/utility.test.js
@@ -277,33 +277,6 @@ describe('Utility features', () => {
       })
   })
 
-  it('should be able to provide Nightwatch client as step definition first parameter', () => {
-    return testCaseFactory
-      .create('test-nightwatch-client-as-parameter')
-      .feature('addition')
-      .scenario('small numbers')
-      .given('User is on the simple calculator page', () => client.init())
-      .and('User enter "4" in A field', (a) => client.setValue('#a', a))
-      .and('User enter "5" in B field', (b) => client.setValue('#b', b))
-      .when('User press Add button', () => client.click('#add'))
-      .then('The result should contain "9"', (result) => client.assert.containsText('#result', result))
-      .scenario('big numbers')
-      .given('User is on the simple calculator page')
-      .and('User enter "44" in A field')
-      .and('User enter "55" in B field')
-      .when('User press Add button')
-      .then('The result should contain "99"')
-      .run()
-      .then((result) => {
-        result.features[0].result.status.should.be.passed
-        result.features[0].result.scenarioCounts.should.deep.equal({passed: 2})
-        result.features[0].scenarios[0].result.status.should.be.passed
-        result.features[0].scenarios[0].result.stepCounts.should.deep.equal({passed: 5})
-        result.features[0].scenarios[1].result.status.should.be.passed
-        result.features[0].scenarios[1].result.stepCounts.should.deep.equal({passed: 5})
-      })
-  })
-
   it('should work together with babel', () => {
     return testCaseFactory
       .create('babel-example', { babel: true })
@@ -311,62 +284,19 @@ describe('Utility features', () => {
       .scenario('small numbers')
       .given('User is on the simple calculator page', `async () => {
         await client.init()
-      }`)
-      .and('User enter 4 in A field', `async () => {
         await client.setValue('#a', 4)
-      }`)
-      .and('User enter 5 in B field', `async () => {
         await client.setValue('#b', 5)
-      }`)
-      .when('User press Add button', `async () => {
         await client.click('#add')
       }`)
       .then('The result should contain 9', `async () => {
         await client.assert.containsText('#result', 9)
       }`)
-      .scenario('big numbers')
-      .given('User is on the simple calculator page')
-      .and('User enter 4 in A field')
-      .and('User enter 5 in B field')
-      .when('User press Add button')
-      .then('The result should contain 9')
-      .feature('subtraction')
-      .scenario('small numbers')
-      .given('User is on the simple calculator page')
-      .and('User enter 9 in A field', `async () => {
-        await client.setValue('#a', 9)
-      }`)
-      .and('User enter 3 in B field', `async () => {
-        await client.setValue('#b', 3)
-      }`)
-      .when('User press Subtract button', `async () => {
-        await client.click('#subtract')
-      }`)
-      .then('The result should contain 6', `async () => {
-        await client.assert.containsText('#result', 6)
-      }`)
-      .scenario('big numbers')
-      .given('User is on the simple calculator page')
-      .and('User enter 4 in A field')
-      .and('User enter 5 in B field')
-      .when('User press Subtract button')
-      .then('The result should contain -1', `async () => {
-        await client.assert.containsText('#result', -1)
-      }`)
       .run()
       .then((result) => {
         result.features[0].result.status.should.be.passed
-        result.features[0].result.scenarioCounts.should.deep.equal({passed: 2})
+        result.features[0].result.scenarioCounts.should.deep.equal({passed: 1})
         result.features[0].scenarios[0].result.status.should.be.passed
-        result.features[0].scenarios[0].result.stepCounts.should.deep.equal({passed: 5})
-        result.features[0].scenarios[1].result.status.should.be.passed
-        result.features[0].scenarios[1].result.stepCounts.should.deep.equal({passed: 5})
-        result.features[1].result.status.should.be.passed
-        result.features[1].result.scenarioCounts.should.deep.equal({passed: 2})
-        result.features[1].scenarios[0].result.status.should.be.passed
-        result.features[1].scenarios[0].result.stepCounts.should.deep.equal({passed: 5})
-        result.features[1].scenarios[1].result.status.should.be.passed
-        result.features[1].scenarios[1].result.stepCounts.should.deep.equal({passed: 5})
+        result.features[0].scenarios[0].result.stepCounts.should.deep.equal({passed: 2})
       })
   })
 })
