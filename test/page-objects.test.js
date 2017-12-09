dea5b96061b9e3723962adf384744f05849978ff
Igor Muchychka <mucsi_96@yahoo.com>
2017-12-09 11:32:38 +0100

    Update dependencies (#319)
    * Update dependencies
    
    * Add test for usage of client in page object custom commands
    
    * Improve page object tests
    
    * Don't start selenium on TravisCI
    
    * Fix typo
    
    * Change selenium port to 4445
    
    * Use selenium 3.7.1
    
    * Update more deps
    
    * Downgrade to selenium 3.7.1

--- test/page-objects.test.js
+++ test/page-objects.test.js
@@ -3,11 +3,60 @@
 const chai = require('chai')
 chai.should()
 const testCaseFactory = require('./test-case-factory')
+let calculator
 
 describe('Assertion features', () => {
   it('should enable the usage of client in page object custom commands', () => {
     return testCaseFactory
       .create('client-in-page-object-custom-commands-test')
+      .pageObject('calculator', `
+      const commands = {
+        setA: function (value) {
+          return this.setValue('@a', value)
+        },
+        setB: function (value) {
+          return this.setValue('@b', value)
+        },
+        pressAdd: function () {
+          this.api.pause(1000)
+          return this.click('@add')
+        },
+        checkResult: function (expectedResult) {
+          return this.assert.containsText('@result', expectedResult)
+        }
+      }
+      module.exports = {
+        url: 'http://yahoo.com',
+        elements: {
+          body: 'body',
+          a: '#a',
+          b: '#b',
+          add: '#add',
+          result: '#result',
+          searchBar: 'input[name="p"]'
+        },
+        commands: [commands]
+      }`)
+      .feature('addition')
+      .scenario('small numbers')
+      .prependStepDefinition('const calculator = client.page.calculator()')
+      .given('User is on the simple calculator page', () => client.init())
+      .and('User enter 4 in A field', () => calculator.setA(4))
+      .and('User enter 5 in B field', () => calculator.setB(5))
+      .when('User press Add button', () => calculator.pressAdd())
+      .then('The result should contain 9', () => calculator.checkResult(9))
+      .run()
+      .then((result) => {
+        result.features[0].result.status.should.be.passed
+        result.features[0].result.scenarioCounts.should.deep.equal({passed: 1})
+        result.features[0].scenarios[0].result.status.should.be.passed
+        result.features[0].scenarios[0].result.stepCounts.should.deep.equal({passed: 5})
+      })
+  })
+
+  it('should enable the usage of shared client in page object custom commands', () => {
+    return testCaseFactory
+      .create('shared-client-in-page-object-custom-commands-test')
       .pageObject('shared', `module.exports = {
         url: 'http://yahoo.com',
         elements: {
@@ -44,11 +93,12 @@ describe('Assertion features', () => {
       }`)
       .feature('addition')
       .scenario('small numbers')
+      .prependStepDefinition('const calculator = client.page.calculator()')
       .given('User is on the simple calculator page', () => client.init())
-      .and('User enter 4 in A field', () => client.page.calculator().setA(4))
-      .and('User enter 5 in B field', () => client.page.calculator().setB(5))
-      .when('User press Add button', () => client.page.calculator().pressAdd())
-      .then('The result should contain 9', () => client.page.calculator().checkResult(9))
+      .and('User enter 4 in A field', () => calculator.setA(4))
+      .and('User enter 5 in B field', () => calculator.setB(5))
+      .when('User press Add button', () => calculator.pressAdd())
+      .then('The result should contain 9', () => calculator.checkResult(9))
       .run()
       .then((result) => {
         result.features[0].result.status.should.be.passed
@@ -97,11 +147,12 @@ describe('Assertion features', () => {
       }`)
       .feature('addition')
       .scenario('small numbers')
+      .prependStepDefinition('const calculator = client.page.calculator()')
       .given('User is on the simple calculator page', () => client.init())
-      .and('User enter 4 in A field', () => client.page.calculator().setA(4))
-      .and('User enter 5 in B field', () => client.page.calculator().setB(5))
-      .when('User press Add button', () => client.page.calculator().pressAdd())
-      .then('The result should contain 9', () => client.page.calculator().checkResult(9))
+      .and('User enter 4 in A field', () => calculator.setA(4))
+      .and('User enter 5 in B field', () => calculator.setB(5))
+      .when('User press Add button', () => calculator.pressAdd())
+      .then('The result should contain 9', () => calculator.checkResult(9))
       .run()
       .then((result) => {
         result.features[0].result.status.should.be.passed
