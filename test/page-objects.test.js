dc341e527bc7f41351ffbdb9e898bab0f3c6f003
Igor Mucsicska <mucsi_96@yahoo.com>
2018-01-30 07:26:07 +0100

    Add Section cinstructor to API

--- test/page-objects.test.js
+++ test/page-objects.test.js
@@ -161,4 +161,59 @@ describe('Assertion features', () => {
         result.features[0].scenarios[0].result.stepCounts.should.deep.equal({passed: 5})
       })
   })
+
+  it('should enable the usage of section constructor', () => {
+    return testCaseFactory
+      .create('section-constructor-test')
+      .pageObject('calculator', `
+      const { Section } = require('../../../lib/index')
+      const commands = {
+        getDynamicSection(expectedResult) {
+          return new Section({
+            name: 'Dynamic Section',
+            parent: this,
+            selector: 'body',
+            commands: [{
+              setA: function (value) {
+                return this.setValue('#a', value)
+              },
+              setB: function (value) {
+                return this.setValue('#b', value)
+              },
+              pressAdd: function () {
+                return this.click('#add')
+              },
+              checkResult: function () {
+                return this.assert.containsText('#result', expectedResult)
+              }
+            }]
+          })
+        }
+      }
+      module.exports = {
+        url: 'http://yahoo.com',
+        elements: {
+          body: 'body',
+          searchBar: 'input[name="p"]'
+        },
+        commands: [commands]
+      }`)
+      .feature('addition')
+      .scenario('small numbers')
+      .prependStepDefinition(`
+          const calculator = client.page.calculator();
+          const dynamicSection = calculator.getDynamicSection(9);
+      `)
+      .given('User is on the simple calculator page', () => client.init())
+      .and('User enter values', () => dynamicSection.setA(4)
+                                .then(() => dynamicSection.setB(5))
+                                .then(() => dynamicSection.pressAdd()))
+      .then('The result should contain 9', () => dynamicSection.checkResult())
+      .then((result) => {
+        result.features[0].result.status.should.be.passed
+        result.features[0].result.scenarioCounts.should.deep.equal({passed: 1})
+        result.features[0].scenarios[0].result.status.should.be.passed
+        result.features[0].scenarios[0].result.stepCounts.should.deep.equal({passed: 3})
+      })
+  })
 })
